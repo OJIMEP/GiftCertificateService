8546f3163a5e0d5faea1f59cff36d68c84ff592d
Андрей Бородавко <a.borodavko@21vek.by>
2022-06-29 16:50:52 +0300

    Add project files.

---
+++ SQLQueries/SQLQueryCertInfo.sql
@@ -0,0 +1,59 @@
+Declare @Barcode nvarchar(11);
+
+Set
+	@Barcode = 'CC13AVC5YRW';
+
+SELECT
+	[_IDRRef],
+	[_Fld4242] 
+Into #Temp_CertRef
+FROM
+	[triovist].[dbo].[_Reference172] 
+where
+	[_Fld4242] IN(@Barcode);
+
+Select
+	T1._Fld14496RRef AS CertRef,
+	T1._Fld14497RRef AS CertStatus 
+Into #Temp_CertStatus
+From
+	[triovist].[dbo].[_InfoRg14495] T1 --cert history
+	Inner join (
+		SELECT
+			Max([_Period]) AS [_Period],
+			[_Fld14496RRef]
+		FROM
+			[triovist].[dbo].[_InfoRg14495]
+			Inner Join #Temp_CertRef On _Fld14496RRef = [_IDRRef] 
+		Where
+			[_Active] = 0x01
+			And [_Period] <= Dateadd(Year, 2000, GETDATE())
+		group by
+			[_Fld14496RRef]
+			) T2 On T1._Fld14496RRef = T2._Fld14496RRef
+			And T1.[_Period] = T2.[_Period]
+		Where
+			_Fld14497RRef IN (--filter by status
+				0xB3A1D155BEA215A74F77177CEA264869, 
+				0x8EABEBCCF5A9FBF74BCB7DA9464028AE
+			);
+
+SELECT
+	[_Fld4242] AS Barcode,
+	Sum([_Fld16861]) AS SumLeft
+FROM
+	#Temp_CertRef
+	Inner join [triovist].[dbo].[_AccumRgT16863] SumRemains
+		On SumRemains.[_Fld16860RRef] = #Temp_CertRef.[_IDRRef]
+	Inner join #Temp_CertStatus 
+		On SumRemains.[_Fld16860RRef] = #Temp_CertStatus.CertRef
+Where
+	[_Period] = '5999-11-01T00:00:00'
+Group by
+	[_Fld16860RRef],
+	[_Fld4242],
+	CertStatus
+Having Sum([_Fld16861]) > 0;
+
+Drop table #Temp_CertRef
+Drop table #Temp_CertStatus
